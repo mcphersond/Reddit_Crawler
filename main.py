133452cbc4f191bebe97ff01dc956a5893835592
Scariest Fish <mcphersond13@gmail.com>
2020-02-06 09:42:04 -0500

    Added naptime function for all my sleeps
    Cleaned up code a little by adding naptime

--- main.py
+++ main.py
@@ -43,6 +43,33 @@ def sub_exists(sub):
     return exists
 
 
+#Passing 1 puts on standard 60 sec nap.
+#Passing 2 puts on nap with the message about pissing off reddit
+#Passing 3 is a time out.
+#Passing 4 is the long nap, 30 minutes.
+def naptime(choice):
+    if choice == 1:
+        print("Reached maximum requests per minute.. taking a nap.")
+        time.sleep(60)
+        print("Resuming...")
+        return 0
+    elif choice == 2:
+        print("Looks like we pissed off reddit. Starting one minute break early!")
+        time.sleep(60)
+        print("Resuming...")
+        return 0
+    elif choice == 3:
+        print("This request failed... Placing on 15 second cooldown.")
+        time.sleep(15)
+        print("And we're back!")
+        return 0
+    elif choice == 4:
+        print("Going to sleep...")
+        time.sleep(1800)
+        print("Waking up...")
+        return 0
+
+
 MAIN_DIR = str(Path.home()) + '\\Pictures\\Reddit Crawler'
 
 # Creates main directory if does not exist
@@ -52,20 +79,16 @@ if not os.path.isdir(MAIN_DIR):
 #Creating reddit session
 user_agent = 'Reddit Crawler 1.2 by /u/Gordramus'
 reddit = praw.Reddit('bot1', user_agent=user_agent)
+
 sub_file = open(r'Subreddits.txt', 'r')
 subreddits = sub_file.read().split(" ")
 sub_file.close()
-first = True
+
 req_count = 0
 while True:
-    if not first:
-        print("Waking up...")
     for subname in subreddits:
         if req_count == 30:
-            print("Reached maximum requests per minute.. taking a nap.")
-            time.sleep(60)
-            req_count = 0
-            print("Resuming...")
+            req_count = naptime(1)
         if sub_exists(subname):
             sub_folder = os.path.join(MAIN_DIR, subname)
             img_count = 0  # How many posts you want to pull
@@ -73,10 +96,7 @@ while True:
                 for post in reddit.subreddit(subname).hot(limit=50):
                     req_count += 1
                     if req_count == 30:
-                        print("Reached maximum requests per minute.. taking a nap.")
-                        time.sleep(60)
-                        req_count = 0
-                        print("Resuming...")
+                        req_count = naptime(1)
                     if img_count >= 15:
                         break
                     if not os.path.isdir(sub_folder):
@@ -92,20 +112,13 @@ while True:
                             try:
                                 urllib.request.urlretrieve(url, filename)
                             except:
-                                print("This request failed... Placing on 15 second cooldown.")
-                                time.sleep(15)
-                                print("And we're back!")
+                                naptime(3)
                             else:
                                 img_count += 1
                         else:
                             print(filename + " already exists! Skipping OwO")
             except:
-                print("Looks like we pissed off reddit. Starting one minute break early!")
-                req_count = 0
-                time.sleep(60)
+                req_count = naptime(2)
 
-    print("Going to sleep...")
-    first = False
-    time.sleep(1800)
-
+    req_count = naptime(4)
 
